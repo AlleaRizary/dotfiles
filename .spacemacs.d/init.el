811306ba6177f9a930ceb360ee5521d0e253c6d1
Rizary <andika.riyan@gmail.com>
2016-03-10 00:03:25 +0700

    add configuration.nix and git config

--- .spacemacs.d/init.el
+++ .spacemacs.d/init.el
@@ -271,11 +271,7 @@ in `dotspacemacs/user-config'."
   "Configuration function for user code.
 This function is called at the very end of Spacemacs initialization after
 layers configuration. You are free to put any user code."
-
-  
-
-
-  )
+  ) 
 
 ;; Do not write anything past this comment. This is where Emacs will
 ;; auto-generate custom variable definitions.
